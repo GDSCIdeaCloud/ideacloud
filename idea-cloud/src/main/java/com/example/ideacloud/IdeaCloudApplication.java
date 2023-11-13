f8be255d8cb53b660be98b1464a0973b1984bd98
seonghwa95 <holyflower95@naver.com>
2023-11-13 20:17:48 +0900

    Initial Commit

---
+++ idea-cloud/src/main/java/com/example/ideacloud/IdeaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaCloudApplication.class, args);
+	}
+
+}
